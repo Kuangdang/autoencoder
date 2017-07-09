f6606d9d4313ff41bc66537197a4cfe9a9cdbc95
cun <cun.wang@rwth-aachen.de>
2017-07-09 18:21:39 +0200

    update projection

--- convcell.py
+++ convcell.py
@@ -8,21 +8,28 @@ from IPython.core.debugger import Tracer; debug_here = Tracer()
 
 
 class ConvLSTMCell(RNNCell):
-    def __init__(self, num_units, shape, filter_size, activation=tanh, reuse=None):
+    def __init__(self, num_units, shape, filter_size, num_proj=None, activation=tanh, reuse=None):
         self._num_units = num_units
         self._filter_size = filter_size
         self._shape = shape
+        self._num_proj = num_proj
         self._activation = activation
         self._reuse = reuse
+
+        if self._num_proj:
+            self._output_size = [self._shape[0], self._shape[1], self._num_proj] 
+        else:
+            self._output_size = [self._shape[0], self._shape[1], self._num_units]
 
 
     @property
     def state_size(self):
-        return ([self._shape[0], self._shape[1], self._num_units], [self._shape[0], self._shape[1], self._num_units])
+        return ([self._shape[0], self._shape[1], self._num_units], [self._shape[0], self._shape[1], self._num_proj]) if self._num_proj else ([self._shape[0], self._shape[1], self._num_units], [self._shape[0], self._shape[1], self._num_units])
 
     @property
     def output_size(self):
-        return [self._shape[0], self._shape[1], self._num_units]
+        return self._output_size 
+
 
     def zero_state(self, batch_size, dtype):
         state_size = self.state_size
@@ -38,38 +45,33 @@ class ConvLSTMCell(RNNCell):
         channels = inputs.get_shape()[3] 
         f_h = self._filter_size[0]
         f_w = self._filter_size[1]
-        #test
-        print("input shape", inputs.get_shape())
+
         #input gate
-        print("input gate")
         Wxi = tf.get_variable("Wxi", [f_h, f_w, channels, self._num_units], dtype=dtype)
-        Whi = tf.get_variable("Whi", [f_h, f_w, self._num_units, self._num_units], dtype=dtype)
+        Whi = tf.get_variable("Whi", [f_h, f_w, self._output_size[2], self._num_units], dtype=dtype)
         Wci = tf.get_variable("Wci", [1, in_h, in_w, self._num_units], dtype=dtype)
         bi = tf.get_variable("bi", [1, in_h, in_w, self._num_units], dtype=dtype)
         input_gate = sigmoid(tf.nn.conv2d(inputs, Wxi, [1,1,1,1], "SAME") + tf.nn.conv2d(h, Whi, [1,1,1,1], "SAME")
                 + Wci*c + bi)
 
         #forget gate
-        print("forget gate")
         Wxf = tf.get_variable("Wxf", [f_h, f_w, channels, self._num_units], dtype=dtype)
-        Whf = tf.get_variable("Whf", [f_h, f_w, self._num_units, self._num_units], dtype=dtype)
+        Whf = tf.get_variable("Whf", [f_h, f_w, self._output_size[2], self._num_units] , dtype=dtype)
         Wcf = tf.get_variable("Wcf", [1, in_h, in_w, self._num_units], dtype=dtype)
         bf = tf.get_variable("bf", [1, in_h, in_w, self._num_units], dtype=dtype)
         forget_gate = sigmoid(tf.nn.conv2d(inputs, Wxf, [1,1,1,1], "SAME") + tf.nn.conv2d(h, Whf, [1,1,1,1], "SAME")
                 + Wcf*c + bf)
 
         #new cell state
-        print("new cell state")
         Wxc = tf.get_variable("Wxc", [f_h, f_w, channels, self._num_units], dtype=dtype)
-        Whc = tf.get_variable("Whc", [f_h, f_w, self._num_units, self._num_units], dtype=dtype)
+        Whc = tf.get_variable("Whc", [f_h, f_w, self._output_size[2], self._num_units], dtype=dtype)
         bc = tf.get_variable("bc", [1, in_h, in_w, self._num_units], dtype=dtype)
         new_c = forget_gate * c + input_gate * self._activation(tf.nn.conv2d(inputs, Wxc, [1,1,1,1], "SAME")
                 + tf.nn.conv2d(h, Whc, [1,1,1,1], "SAME") + bc)
 
         #output gate
-        print("output gate ")
         Wxo = tf.get_variable("Wxo", [f_h, f_w, channels, self._num_units], dtype=dtype)
-        Who = tf.get_variable("Who", [f_h, f_w, self._num_units, self._num_units], dtype=dtype)
+        Who = tf.get_variable("Who", [f_h, f_w, self._output_size[2], self._num_units], dtype=dtype)
         Wco = tf.get_variable("Wco", [1, in_h, in_w, self._num_units], dtype=dtype)
         bo = tf.get_variable("bo", [1, in_h, in_w, self._num_units], dtype=dtype)
         output_gate = sigmoid(tf.nn.conv2d(inputs, Wxo, [1,1,1,1], "SAME") + tf.nn.conv2d(h, Who, [1,1,1,1], "SAME")
@@ -77,9 +79,14 @@ class ConvLSTMCell(RNNCell):
 
         #new h
         new_h = output_gate * self._activation(new_c)
+        if self._num_proj:
+            w = tf.Variable(tf.truncated_normal([1, 1, self._output_size[2], channels], -0.1 , 0.1, dtype=tf.float32), name='w')
+            b = tf.Variable(tf.zeros([1, in_h, in_w, channels]), name='b', dtype=tf.float32)
+            new_h = tf.nn.conv2d(new_h, w, [1, 1, 1, 1], "SAME") + b
+        '''
         shape = new_h.get_shape()
         print("output shape", shape)
         new_h.set_shape([None, shape[1], shape[2], shape[3]])
         print("cell finished")
-        debug_here()
+        '''
         return new_h, (new_c, new_h)
