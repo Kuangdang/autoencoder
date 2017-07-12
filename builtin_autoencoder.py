2c8a6ddd0e3cc3aa91a26d9473065255101dd7a6
cun <cun.wang@rwth-aachen.de>
2017-07-12 10:17:10 +0200

    conditioned

--- builtin_autoencoder.py
+++ builtin_autoencoder.py
@@ -1,13 +1,10 @@
 import numpy as np
-#import matplotlib.pyplot as plt
-#import matplotlib.image as mpimg
 import tensorflow as tf
 import sys
-#from lstmcell import LSTMCell
 
 #autoencoder class
 class Autoencoder:
-    def __init__(self, inputs, hidden_num, enc_cell=None, dec_cell=None, optimizer=None):
+    def __init__(self, inputs, hidden_num, enc_cell=None, dec_cell=None, optimizer=None, conditioned=None):
         '''
         inputs shape [maxtime, batch_size, frame_size*frame_size]
         '''
@@ -26,6 +23,7 @@ class Autoencoder:
         with tf.variable_scope('encode', reuse=None):
             _, enc_state = tf.nn.dynamic_rnn(self.enc_cell, inputs, dtype=tf.float32, time_major=True)
 
+        #assum projection is performed in cell
         saved_output = tf.Variable(tf.zeros([batch_size, desired]), trainable=False)
         saved_state = tf.Variable(tf.zeros([batch_size, desired]), trainable=False)
 
@@ -39,7 +37,14 @@ class Autoencoder:
             v = None
             for _ in range(maxtime):
                 with tf.variable_scope('unrolling', reuse=v):
-                    output, dec_state = self.dec_cell(dec_inputs, dec_state)
+                    if conditioned is None:
+                        output, dec_state = self.dec_cell(dec_inputs, dec_state)
+                    else:
+                        if v is None:
+                            output, dec_state = self.dec_cell(dec_inputs, dec_state)
+                        else:
+                            output, dec_state = self.dec_cell(output, dec_state)
+                        
                     v = True
                     outputs.append(output)
 
@@ -67,8 +72,9 @@ class Autoencoder:
 
 if __name__ =='__main__':
     PATH = "/home/stud/wangc/lab/record/"
+    DATASET = "/home/stud/wangc/lab/mnist_test_seq.npy"
     f = open(PATH + "log", "w+")  
-    data = np.load('/home/stud/wangc/lab/mnist_test_seq.npy')
+    data = np.load(DATASET)
     data = np.around(data/255, decimals=5)
     data = data.reshape(data.shape[0],data.shape[1],-1)
     maxtime = data.shape[0]
@@ -76,39 +82,45 @@ if __name__ =='__main__':
     hidden_num = 5000
     batch_size = 50
     train_size = 9000
+    val_size = 500
     test_size = 500
     epoch = 80
     steps = int(train_size/batch_size)
+    val_steps = int(val_size/batch_size)
+    test_steps = int(test_size/batch_size)
+    val_start = train_size
+    test_start = val_start + val_size
 
     inputs = tf.placeholder(tf.float32, shape = [maxtime, batch_size, desired], name='inputs')
     ae = Autoencoder(inputs, hidden_num)
     print("hidden_num %d, batch_size %d, epoch %d, optimizer %s, cell %s" % (hidden_num, batch_size, epoch, ae.optimizer, ae.enc_cell), file=f)
     f.flush()
-    print('create session')
 
     with tf.Session() as sess:
-        print('beginning-------------------------------')
         tf.global_variables_initializer().run()
-        print('initialized')
-        train_writer = tf.summary.FileWriter(
-                PATH + 'logdir'+'/train', sess.graph)
-        validate_writer = tf.summary.FileWriter(
-                PATH + 'logdir'+'/validate', sess.graph) 
-        val_outputs = list()
+        train_writer = tf.summary.FileWriter(PATH + 'logdir'+'/train', sess.graph)
+        validate_writer = tf.summary.FileWriter(PATH + 'logdir'+'/validate', sess.graph) 
+
         for j in range(epoch):
             for i in range(steps):
                 _, train_sum = sess.run([ae.train, ae.loss_sum], feed_dict={inputs:(data[:,i*batch_size:(i+1)*batch_size])})
             train_writer.add_summary(train_sum, j)
-            _, val_sum = sess.run([ae.outputs, ae.loss_sum], feed_dict={inputs:(data[:,train_size:train_size+batch_size])})
-            validate_writer.add_summary(val_sum, j)
-        val_outputs = np.array(val_outputs)
+
+            val_loss_sum = 0
+            for p in range(val_steps): 
+                _, val_sum, val_loss = sess.run([ae.outputs, ae.loss_sum, ae.loss],
+                        feed_dict={inputs:(data[:,val_start+p*batch_size:val_start+(p+1)*batch_size])})
+                val_loss_sum += val_loss
+            val_avrg = val_loss_sum/val_steps
+            val_summa = tf.summary.scalar("val_summa", val_avrg)
+            validate_writer.add_summary(val_summa, j)
         train_writer.close()
         validate_writer.close()
 
         test_sum = 0
-        test_steps = int(test_size/batch_size)
         for k in range(test_steps):
-            test_outputs, test_l = sess.run([ae.outputs, ae.loss], feed_dict={inputs:data[:,9500+k*batch_size:9500+(k+1)*batch_size]})
+            test_outputs, test_l = sess.run([ae.outputs, ae.loss], 
+                    feed_dict={inputs:data[:,test_start+k*batch_size:test_start+(k+1)*batch_size]})
             test_sum += test_l 
         average_test = test_sum/test_steps
         print("test error %f" % average_test, file=f)
@@ -116,6 +128,5 @@ if __name__ =='__main__':
         np.savez_compressed(PATH + "outputs",
                 test_out=test_outputs, test_in=data[:,-batch_size:-1])
     
-    print('end---------------------------------')
     f.close()
     sys.exit(0)
