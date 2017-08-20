94b453367deeab2b4bf3ec6fe4f0af481a8b7358
cun wang <cun.wang@protonmail.com>
2017-08-20 20:31:10 +0200

    future predictor

--- tools.py
+++ tools.py
@@ -5,3 +5,9 @@ def normalizedata(data):
     min_e = np.amin(data)
     norm = 2 * (data - min_e)/(max_e - min_e) - 1
     return norm
+
+def denormdata(data):
+    max_e = np.amax(data)
+    min_e = np.amin(data)
+    norm = 255 * (data -min_e)/(max_e -min_e)
+    return norm
